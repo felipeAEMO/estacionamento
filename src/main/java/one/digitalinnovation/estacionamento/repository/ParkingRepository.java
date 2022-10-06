45b51e1844294fe4b6b097e73924ee61aaefe948
felipeAEM <58379228+felipeAEMO@users.noreply.github.com>
2022-10-05 21:23:43 -0300

    Delete ParkingRepository.java

--- src/main/java/one/digitalinnovation/estacionamento/repository/ParkingRepository.java
+++
@@ -1,9 +0,0 @@
-package one.digitalinnovation.estacionamento.repository;
-
-import one.digitalinnovation.estacionamento.model.Parking;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ParkingRepository extends JpaRepository<Parking, String> {
-}
